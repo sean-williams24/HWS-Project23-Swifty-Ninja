3897b22d4773159fc7f56331a690ca22dc981f6b
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-31 15:59:01 +0000

    FEAT: Create a bezier path and draw slices along path removing old ones to keep a maximum of 12.

--- Project 23 - Swifty Ninja/GameScene.swift
+++ Project 23 - Swifty Ninja/GameScene.swift
@@ -94,22 +94,49 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
+        // 1 Remove all existing points in the activeSlicePoints array, because we're starting fresh.
         activeSlicePoints.removeAll(keepingCapacity: true)
-        
+
+        // 2 Get the touch location and add it to the activeSlicePoints array.
         let location = touch.location(in: self)
         activeSlicePoints.append(location)
-        
+
+        // 3 Call the redrawActiveSlice() method to clear the slice shapes.
         redrawActiveSlice()
-        
+
+        // 4 Remove any actions that are currently attached to the slice shapes. This will be important if they are in the middle of a fadeOut(withDuration:) action.
         activeSliceBG.removeAllActions()
         activeSliceFG.removeAllActions()
-        
+
+        // 5 Set both slice shapes to have an alpha value of 1 so they are fully visible.
         activeSliceBG.alpha = 1
         activeSliceFG.alpha = 1
     }
     
     func redrawActiveSlice() {
-        
+        // 1 If we have fewer than two points in our array, we don't have enough data to draw a line so it needs to clear the shapes and exit the method.
+        if activeSlicePoints.count < 2 {
+            activeSliceBG.path = nil
+            activeSliceFG.path = nil
+            return
+        }
+
+        // 2 If we have more than 12 slice points in our array, we need to remove the oldest ones until we have at most 12 – this stops the swipe shapes from becoming too long.
+        if activeSlicePoints.count > 12 {
+            activeSlicePoints.removeFirst(activeSlicePoints.count - 12)
+        }
+
+        // 3 It needs to start its line at the position of the first swipe point, then go through each of the others drawing lines to each point.
+        let path = UIBezierPath()
+        path.move(to: activeSlicePoints[0])
+
+        for i in 1 ..< activeSlicePoints.count {
+            path.addLine(to: activeSlicePoints[i])
+        }
+
+        // 4 Finally, it needs to update the slice shape paths so they get drawn using their designs – i.e., line width and color.
+        activeSliceBG.path = path.cgPath
+        activeSliceFG.path = path.cgPath
     }
     
 }
